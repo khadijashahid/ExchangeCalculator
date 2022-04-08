9dfaebfbf0cccedd0137b1f6729c896f33c74076
Khadija Shahid <khadija_shahid3197@outlook.com>
2022-04-08 14:12:31 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React,{useEffect, useState} from 'react';
-
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faExchangeAlt} from '@fortawesome/free-solid-svg-icons';
 import './App.css';
-
 import Currency from './Currency';
+<script src="https://kit.fontawesome.com/51ff4e98b4.js" crossorigin="anonymous"></script>
 
-//const BASE_URL = "https://xch-api.herokuapp.com"
+// const BASE_URL = "https://xch-api.herokuapp.com"
 
 const BASE_URL = "https://xch-api.herokuapp.com/rates"
 
 function App() {
+  const [currencyName, setCurrencyName] = useState('')
   const [currencyOptions, setCurrencyOptions] = useState([])
   const [fromCurrency, setFromCurrency] = useState()
   const [toCurrency, setToCurrency] = useState()
   const [exchangeRate, setExchangeRate] = useState()
-  const [amount, setAmount] = useState()
-  const [amountInfromCurrenncy, setAmountInFromCurrency ] = useState(true)
+  const [amount, setAmount] = useState(1)
+  const [amountInfromCurrency, setAmountInFromCurrency ] = useState(true)
+  console.log(exchangeRate)
 
 
   let toAmount, fromAmount
-  if (amountInfromCurrenncy){
+  if (amountInfromCurrency){
     fromAmount = amount
     toAmount = amount * exchangeRate
   } else {
@@ -26,22 +29,30 @@ function App() {
     toAmount = amount
     fromAmount = amount / exchangeRate
   }
+  useEffect(() => {
+    fetch("https://xch-api.herokuapp.com")
+        .then(response => response.json())
+        .then(data => {
+            setCurrencyName(data.currencies)
+        })
+}, [])
   
 
   useEffect(()=> {
     fetch(BASE_URL)
     .then(res => res.json())
     .then(data => {
-      const firstCurrency = Object.keys(data.rates)[0]
-      const secondCurrency = Object.keys(data.rates)[1]
-      setCurrencyOptions([data.base, ...Object.keys(data.rates)])
-      setFromCurrency(data.base)
-      setToCurrency(firstCurrency)
-      setFromCurrency(secondCurrency)
+      console.log(data)
+      const firstCurrency = Object.keys(data.rates)[1]
+      const secondCurrency = Object.keys(data.rates)[0]
+      setCurrencyOptions([data.success,...Object.keys(data.rates)])
+      setFromCurrency(firstCurrency)
+      setToCurrency(secondCurrency)
       setExchangeRate(data.rates[firstCurrency])
-      setExchangeRate(data.rates[secondCurrency])
     })
   }, [])
+
+
 
   function handleFromAmountChange(e){
     setAmount(e.target.value)
@@ -57,6 +68,7 @@ function App() {
   <div className="wrapper">
     <div className="container">
    <header className="header"> Calculator </header>
+   { console.log("currencyName", currencyName) }
   
     <Currency
     currencyOptions={currencyOptions}
@@ -65,6 +77,16 @@ function App() {
     onChangeAmount = {handleFromAmountChange}
     amount={fromAmount}
     />
+    <div className="icon"> <FontAwesomeIcon icon={faExchangeAlt}> </FontAwesomeIcon> </div>
+
+     <Currency
+    currencyOptions={currencyOptions}
+    selectedCurrency={toCurrency}
+    onChangeCurrency = {e => setToCurrency(e.target.value) }
+    onChangeAmount = {handleToAmountChange}
+    amount={toAmount}
+    />
+    
     </div>
     </div>
     
--- src/Currency.js
+++ src/Currency.js
@@ -1,63 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faExchangeAlt} from '@fortawesome/free-solid-svg-icons';
+import React from "react";
 
-<script src="https://kit.fontawesome.com/51ff4e98b4.js" crossorigin="anonymous"></script>
+
+
 export default function Currency(props) {
-    const [currencyName, setCurrencyName] = useState('')
-
+   
     const {
 
         currencyOptions,
         selectedCurrency,
         onChangeCurrency,
-        currencies,
+        onChangeAmount,
         amount
     } = props
 
-    useEffect(() => {
-        fetch("https://xch-api.herokuapp.com")
-            .then(response => response.json())
-            .then(data => {
-                setCurrencyName(data.currencies)
-            })
-    }, [])
+ 
 
     return (
         <div className="row">
             <div className="col-md-6">
-            {
-                    console.log("currencyName", currencyName.crypto)
-                }
-                {
-                    console.log("currencyName", currencyName)
-                }
          
-                 <input type="number" className="inp1" value={amount}  />
+                 <input type="number" className="inp1" value={amount} onChange={onChangeAmount} />
             <select className="sel1" value={selectedCurrency} onChange={onChangeCurrency} >
                 {
                currencyOptions.map(currencies => (
                             <option key={currencies} value={currencies}>
                                
-                                {currencies}
+                               {currencies}
                             </option>
                 )
             )}
 
             </select>
             <br/>
-            <div className="icon"> <FontAwesomeIcon icon={faExchangeAlt}> </FontAwesomeIcon> </div>
-            <input type="number" className="inp1" value={amount}  />
-            <select className="sel1" value={selectedCurrency} onChange={onChangeCurrency} >
-                {
-               currencyOptions.map(currencies => (
-                            <option key={currencies} value={currencies}>
-                                {currencies}
-                            </option>
-                )
-            )}
-
-            </select>
             </div>
             </div>
            
